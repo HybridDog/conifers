9eac811d72b469a8dbc5dd5d31fa30db012d4f1e
HybridDog <adresse_test@t-online.de>
2013-10-27 11:49:44 +0100

    update

--- init.lua
+++ init.lua
@@ -217,16 +217,14 @@ minetest.register_craft({
 minetest.register_abm({
 	nodenames = "default:dirt_with_grass",
 	interval = INTERVAL,
-	chance = 1,
+	chance = 200.2,
 	
-	action = function(pos, node, _, _)
-   		if minetest.get_node({x = pos.x, y = pos.y + 1, z = pos.z}).name == "air"
-   			and pos.y >= CONIFERS_ALTITUDE
-   			and conifers:is_node_in_cube({"conifers:trunk"}, pos, CONIFERS_DISTANCE) == false
-   		then
-   			if math.random(0,1000) == 5 then
-   				conifers:make_conifer({x = pos.x, y = pos.y + 1, z = pos.z}, math.random(0, 1))
-   			end
+	action = function(pos, node)
+		local p = {x=pos.x, y=pos.y+1, z=pos.z}
+   		if minetest.get_node(p).name == "air"
+   		and pos.y >= CONIFERS_ALTITUDE
+   		and (not conifers:is_node_in_cube({"conifers:trunk"}, pos, CONIFERS_DISTANCE)) then
+   			conifers:make_conifer(p, math.random(0, 1))
 		end
     end
 })
@@ -277,8 +275,8 @@ end
 -- Taken from the Flowers mod by erlehmann.
 --
 function conifers:table_contains(t, v)
-    for _, i in ipairs(t) do
-		if (i == v) then
+    for _,i in ipairs(t) do
+		if i == v then
 			return true
 		end
     end
@@ -290,14 +288,15 @@ end
 --
 -- Taken from the Flowers mod by erlehmann.
 --
-function conifers:is_node_in_cube(nodenames, node_pos, radius)
-    for x = node_pos.x - radius, node_pos.x + radius do
-		for y = node_pos.y - math.floor(radius / 2), node_pos.y + math.floor(radius / 2) do
-			for z = node_pos.z - radius, node_pos.z + radius do
-				n = minetest.get_node_or_nil({x = x, y = y, z = z})
-				if (n == nil)
-					or (n.name == 'ignore')
-					or (conifers:table_contains(nodenames, n.name) == true) then
+function conifers:is_node_in_cube(nodenames, pos, size)
+	local hs = math.floor(size / 2)
+    for x = pos.x-size, pos.x+size do
+		for y = pos.y-hs, pos.y+hs do
+			for z = pos.z-size, pos.z+size do
+				n = minetest.get_node_or_nil({x=x, y=y, z=z})
+				if n == nil
+				or n.name == 'ignore'
+				or conifers:table_contains(nodenames, n.name) then
 					return true
 				end
 			end
@@ -323,10 +322,6 @@ function conifers:are_leaves_surrounded(pos)
 	--
 	-- Check if a leaves block does not interfer with something else than the air or another leaves block.
 	--
-	local node1 = minetest.get_node({x = pos.x + 1, y = pos.y, z = pos.z}).name
-	local node2 = minetest.get_node({x = pos.x - 1, y = pos.y, z = pos.z}).name
-	local node3 = minetest.get_node({x = pos.x, y = pos.y, z = pos.z + 1}).name
-	local node4 = minetest.get_node({x = pos.x, y = pos.y, z = pos.z - 1}).name
 	local replacable_nodes = {
 		"air", 
 		"conifers:leaves", 
@@ -334,15 +329,14 @@ function conifers:are_leaves_surrounded(pos)
 	}
 
 	-- Let's check if the neighboring node is a replacable node.
-	if (conifers:table_contains(replacable_nodes, node1) == true)
-		and (conifers:table_contains(replacable_nodes, node2) == true)
-		and (conifers:table_contains(replacable_nodes, node3) == true)
-		and (conifers:table_contains(replacable_nodes, node4) == true)
-	then
-		return false
-	else
-		return true
+	for i = -1,1,2 do
+		if not (conifers:table_contains(replacable_nodes, minetest.get_node({x=pos.x+i, y=pos.y, z=pos.z}).name)
+			or conifers:table_contains(replacable_nodes, minetest.get_node({x=pos.x, y=pos.y, z=pos.z+i}).name)
+		) then
+			return true
+		end
 	end
+	return false
 end
 
 --
@@ -355,7 +349,8 @@ end
 --	1: bright leaves (special)
 --
 function conifers:add_leaves_block(pos, special, near_trunk)
-	if conifers:are_leaves_surrounded(pos) == false or near_trunk == true then
+	if (not conifers:are_leaves_surrounded(pos))
+	or near_trunk then
 		if special == 0 then
 			minetest.add_node(pos , { name = "conifers:leaves" })
 		else
@@ -439,11 +434,11 @@ end
 --
 function conifers:make_conifer(pos, conifer_type)
 	-- Check if we can gros a conifer at this place.
-	if minetest.get_node({x = pos.x, y = pos.y - 1, z = pos.z}).name ~= "default:dirt_with_grass"
-		and (minetest.get_node({x = pos.x, y = pos.y, z = pos.z}).name ~= "air"
-			or minetest.get_node({x = pos.x, y = pos.y, z = pos.z}).name ~= "conifers:sapling"
-		)
-	then
+	local node = minetest.get_node(pos).name
+	if minetest.get_node({x=pos.x, y=pos.y-1, z=pos.z}).name ~= "default:dirt_with_grass"
+	and (node ~= "air"
+		or node ~= "conifers:sapling"
+	) then
 		return false
 	--else
 		--if minetest.get_node({x = pos.x, y = pos.y, z = pos.z}).name == "conifers:sapling" then
@@ -452,45 +447,44 @@ function conifers:make_conifer(pos, conifer_type)
 	end
 	
 	local height = math.random(TRUNK_MINHEIGHT, TRUNK_MAXHEIGHT) -- Random height of the conifer.
+
+	-- Let's check if we can grow a tree here.
+	-- That means, we must have a column of 'height' high which contains
+	-- only air.
+	for j = 1, height - 1 do -- Start from 1 so we can grow a sapling.
+		if minetest.get_node({x=pos.x, y=pos.y+j, z=pos.z}).name ~= "air" then
+			-- Abort
+			return false
+		end
+	end
+
+	local t1 = os.clock()
+
 	local leaves_height = math.random(LEAVES_MINHEIGHT, LEAVES_MAXHEIGHT) -- Level from where the leaves grow.
 	local current_block = {} -- Duh...
 	local leaves_radius = 1
 	local leaves_max_radius = 2
 	local special = math.random(0, 1)
 
-	-- Let's check if we can grow a tree here.
-	-- That means, we must have a column of 'height' high which contains
-	-- only air.
-	for j = 1, height - 1 do -- Start from 1 so we can grow a sapling.
-		if minetest.get_node({x = pos.x, y = pos.y + j, z = pos.z}).name ~= "air" then
-			-- Abort
-			return false
-		end
-	end
-
 	-- Create the trunk and add the leaves.
 	for i = 0, height - 1 do
-		current_block = {
-			x = pos.x,
-			y = pos.y + i,
-			z = pos.z
-		}
+		current_block = {x=pos.x, y=pos.y+i, z=pos.z}
 		-- Put a trunk block.
 		minetest.add_node(current_block , {name = "conifers:trunk"})
 		-- Put some leaves.
 		if i >= leaves_height then
 			-- Put some leaves.
-			conifers:make_leaves({x = pos.x,	y = pos.y + leaves_height + (height - 1) - i, z = pos.z}, 1, leaves_radius, special)
+			conifers:make_leaves({x=pos.x, y=pos.y+leaves_height+height-1-i, z=pos.z}, 1, leaves_radius, special)
 			--
 			-- TYPE OF CONIFER
 			--
 			if conifer_type == 1 then -- Regular type
 				-- Prepare the next circle of leaves.
-				leaves_radius = leaves_radius + 1
+				leaves_radius = leaves_radius+1
 				-- Check if the current radius is the maximum radius at this level.
 				if leaves_radius > leaves_max_radius then
 					leaves_radius = 1
-					leaves_max_radius = leaves_max_radius + 1
+					leaves_max_radius = leaves_max_radius+1
 					-- Does it exceeds the maximum radius?
 					if leaves_max_radius > LEAVES_MAXRADIUS then
 						leaves_max_radius = LEAVES_MAXRADIUS
@@ -498,20 +492,19 @@ function conifers:make_conifer(pos, conifer_type)
 				end
 			else -- Narrow type
 				if i % 2 == 0 then
-					leaves_radius = LEAVES_NARROWRADIUS - math.random(0, 1)
+					leaves_radius = LEAVES_NARROWRADIUS-math.random(0,1)
 				else
-					leaves_radius = math.floor(LEAVES_NARROWRADIUS / 2)
+					leaves_radius = math.floor(LEAVES_NARROWRADIUS/2)
 				end
 			end
 		end
 	end
 
 	-- Put a top leaves block.
-	current_block.y = current_block.y + 1
+	current_block.y = current_block.y+1
 	conifers:add_leaves_block(current_block, special)
 
 	-- Blahblahblah
-	print ('[conifers] A conifer has grown at (' .. pos.x .. ',' .. pos.y .. ',' .. pos.z .. ') with a height of ' .. (height))
-	
+	print (string.format('[conifers] A conifer has grown at ('..pos.x..','..pos.y..','..pos.z..') with a height of '..height..' after ca. %.2fs', os.clock() - t1))
 	return true
 end
